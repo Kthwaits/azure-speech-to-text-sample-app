9e1cde4e91b293c14072f247525beed5ba26883b
Kevin <kevinthwaits@microsoft.com>
2024-04-10 12:45:16 -0400

    Fixed editing functionality

--- client/src/App.js
+++ client/src/App.js
@@ -15,69 +15,76 @@ function App() {
   const [recordingActive, setRecordingActive] = useState(false);
 
   function onRecognizing(sender, recognitionEventArgs) {
-    var result = recognitionEventArgs.result;
-    setInterimText(result.text);
+    const resultText = recognitionEventArgs?.result?.text;
+    if (resultText) {
+      setInterimText(resultText);
+    }
   }
 
   function onRecognized(sender, recognitionEventArgs) {
-    var result = recognitionEventArgs.result;
-    setDisplayText(prevDisplayText => prevDisplayText + ' ' + result.text);
-    setInterimText('');
-  }
-  function onSessionStarted(sender, sessionEventArgs) {
-    setRecordingActive(true);
-  }
-  function onSessionStopped(sender, sessionEventArgs) {
-    setRecordingActive(false);
-  }
-
-
-  async function sttFromMic() {
-    if (!recordingActive) {
-      const tokenObj = await getTokenOrRefresh();
-      const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
-      speechConfig.speechRecognitionLanguage = 'en-US';
-      const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
-      recognizerRef.current = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
-      recognizerRef.current.recognizing = onRecognizing;
-      recognizerRef.current.recognized = onRecognized;
-      recognizerRef.current.sessionStarted = onSessionStarted;
-      recognizerRef.current.sessionStopped = onSessionStopped;
-      recognizerRef.current.startContinuousRecognitionAsync()
-    }
-    else {
-      if (recognizerRef.current) {
-        recognizerRef.current.stopContinuousRecognitionAsync(
-          function () {
-            recognizerRef.current.close();
-            recognizerRef.current = null; // Set recognizer to null after closing
-          },
-          function (err) {
-            console.log('ERROR: ' + err);
-          }
-        );
-      }
+    const resultText = recognitionEventArgs?.result?.text;
+    if (resultText) {
+      setDisplayText(prevDisplayText => `${prevDisplayText} ${resultText}`);
+      setInterimText('');
     }
   }
 
-  return (
-    <div className="App">
-      <IconButton size="large" onClick={sttFromMic}>
-        {recordingActive ? <StopCircleIcon fontSize="inherit" /> : <MicIcon fontSize="inherit" />}
-      </IconButton>
-      <TextField
-          fullWidth
-          id="outlined-multiline-flexible"
-          label="Speech Output"
-          multiline
-          rows={4}
-          value={displayText + ' ' + interimText}
-          onChange={(event) => {
-            setDisplayText(event.target.value);
-          }}
-        />
-    </div>
-  );
+
+
+function onSessionStarted(sender, sessionEventArgs) {
+  setRecordingActive(true);
+}
+function onSessionStopped(sender, sessionEventArgs) {
+  setRecordingActive(false);
+}
+
+
+async function sttFromMic() {
+  if (!recordingActive) {
+    const tokenObj = await getTokenOrRefresh();
+    const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
+    speechConfig.speechRecognitionLanguage = 'en-US';
+    const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
+    recognizerRef.current = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+    recognizerRef.current.recognizing = onRecognizing;
+    recognizerRef.current.recognized = onRecognized;
+    recognizerRef.current.sessionStarted = onSessionStarted;
+    recognizerRef.current.sessionStopped = onSessionStopped;
+    recognizerRef.current.startContinuousRecognitionAsync()
+  }
+  else {
+    if (recognizerRef.current) {
+      recognizerRef.current.stopContinuousRecognitionAsync(
+        function () {
+          recognizerRef.current.close();
+          recognizerRef.current = null; // Set recognizer to null after closing
+        },
+        function (err) {
+          console.log('ERROR: ' + err);
+        }
+      );
+    }
+  }
+}
+
+return (
+  <div className="App">
+    <IconButton size="large" onClick={sttFromMic}>
+      {recordingActive ? <StopCircleIcon fontSize="inherit" /> : <MicIcon fontSize="inherit" />}
+    </IconButton>
+    <TextField
+      fullWidth
+      id="outlined-multiline-flexible"
+      label="Speech Output"
+      multiline
+      rows={4}
+      value={recordingActive ? displayText + ' ' + interimText : displayText}
+      onChange={(event) => {
+        setDisplayText(event.target.value);
+      }}
+    />
+  </div>
+);
 }
 
 export default App;
